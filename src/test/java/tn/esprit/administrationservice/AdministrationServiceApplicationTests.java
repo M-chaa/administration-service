47095a38c1ef63cd5574855e2cb79e40356840f0
M-chaa <chaabouni.mohamed@esprit.com>
2026-04-29 19:51:11 +0100

    Fix CI Jenkins + H2 tests

--- src/test/java/tn/esprit/administrationservice/AdministrationServiceApplicationTests.java
+++ src/test/java/tn/esprit/administrationservice/AdministrationServiceApplicationTests.java
@@ -2,7 +2,9 @@ package tn.esprit.administrationservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class AdministrationServiceApplicationTests {
 
